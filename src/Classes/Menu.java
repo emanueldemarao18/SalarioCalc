96c89e195f2ee80f00f77eefe761fc6d2200919e
Emanuel _de_Marao <emanueldemarao20@gmail.com>
2022-11-09 13:50:20 +0100

    Adicionando menu

---
+++ src/Classes/Menu.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import java.util.Scanner;
+
+public class Menu {
+	Scanner read = new Scanner(System.in);
+
+	public void tela_inicial() {
+		System.out.println("Digite o tipo de funcionário");
+		System.out.println("1-GERENTE"+"2-GESTOR_CAIXA"+"3-Técnico");
+
+	}
+}
